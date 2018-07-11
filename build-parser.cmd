b4cfbda293809ef646db7b6605f71774ae5ac727
Sergiy Samborskiy <salterok@gmail.com>
2018-07-12 02:01:54 +0300

    Disable antlr listener generation

--- build-parser.cmd
+++ build-parser.cmd
@@ -2,8 +2,8 @@ rem @echo off
 
 rmdir "./out/parser" /s /q
 
-call ./node_modules/.bin/antlr4ts -visitor -o ./src/parser ./src/JapeLexer.g4
-call ./node_modules/.bin/antlr4ts -visitor -o ./src/parser ./src/JapeParser.g4
+call ./node_modules/.bin/antlr4ts -visitor -no-listener -o ./src/parser ./src/JapeLexer.g4
+call ./node_modules/.bin/antlr4ts -visitor -no-listener -o ./src/parser ./src/JapeParser.g4
 
 rem call antlr4 -Dlanguage=JavaScript -visitor -o ./out/parser ./src/JapeLexer.g4
 rem call antlr4 -Dlanguage=JavaScript -visitor -o ./out/parser ./src/JapeParser.g4
