de0bd3c79ebe99d6a14190d33cdf247c9a8d20a5
Sergiy Samborskiy <salterok@gmail.com>
2018-09-27 02:24:26 +0300

    Fix issue with errors without message

--- src/ErrorPatcher.js
+++ src/ErrorPatcher.js
@@ -1,18 +1,10 @@
 
 const RealError = Error;
 
-class ExtendableError extends Error {
-    constructor (message) {
-        super(message);
-        // a workaround to make `instanceof ExtendableError` work in ES5
-        this.constructor = ExtendableError;
-        this.__proto__   = ExtendableError.prototype;
-    }
+function ExtendableError() { 
+    var error = RealError.apply(this, arguments);
+    return error;
 }
+ExtendableError.prototype = new RealError();
 
-function ExtendableError2() { 
-    var returned = RealError.apply(this, arguments);
-}
-ExtendableError2.prototype = new RealError();
-
-global.Error = ExtendableError2;
+global.Error = ExtendableError;
