5d683f09b412315b499fe50625e0dccf1be82679
Sergiy Samborskiy <salterok@gmail.com>
2019-03-13 00:52:36 +0200

    Add some info abount basic features completion
    For now it add in place of completion handling
    but should be moved to some dictionary later.

--- src/providers/JapeCompletionItemProvider.ts
+++ src/providers/JapeCompletionItemProvider.ts
@@ -2,7 +2,7 @@
  * @Author: Sergiy Samborskiy 
  * @Date: 2018-08-08 23:45:29 
  * @Last Modified by: Sergiy Samborskiy
- * @Last Modified time: 2019-02-10 15:29:02
+ * @Last Modified time: 2019-03-13 00:52:25
  */
 
 import { CompletionItemKind, CompletionItem, TextDocument, Position, CancellationToken, CompletionContext, Range } from "vscode-languageserver";
@@ -11,6 +11,7 @@ import * as _ from "lodash";
 import { JapeContext } from "../JapeContext";
 import { JapeLexer } from "../parser/JapeLexer";
 import { translate } from "../utils";
+import { Annotation } from "../JapeSyntaxDefinitions";
 
 export class JapeCompletionItemProvider {
 
@@ -37,6 +38,38 @@ export class JapeCompletionItemProvider {
                     );
                 }
             }
+            if (char === JapeContext.getLiteral(JapeLexer.VIRTUAL_ACCESSOR)) {
+                const annotations: Annotation[] = [
+                    {
+                        name: "base",
+                        features: [
+                            {
+                                name: "length",
+                                value: "",
+                            },
+                            {
+                                name: "string",
+                                value: "",
+                            },
+                            {
+                                name: "cleanString",
+                                value: "",
+                            },
+                        ]
+                    }
+                ];
+                return Promise.resolve(
+                    _.flatten(annotations.map(
+                        a => a.features.map(
+                            feature => {
+                                const item = CompletionItem.create(feature.name);
+                                item.kind = CompletionItemKind.Field;
+                                return item;
+                            }
+                        )
+                    ))
+                );
+            }
             if (char === JapeContext.getLiteral(JapeLexer.ACCESSOR)) {
                 // TODO: check if looking into specific annotation in lhs or into label in rhs
                 const rule = this.japeCtx.findRule(document.uri, position.line);
@@ -48,6 +81,50 @@ export class JapeCompletionItemProvider {
                     }
 
                     const annotations = this.japeCtx.findAnnotations(token.text || "");
+
+                    // TODO: move to dictionary
+                    if (token.text === "Token") {
+                        annotations.push({
+                            name: "base",
+                            features: [
+                                {
+                                    name: "kind",
+                                    value: "",
+                                },
+                                {
+                                    name: "length",
+                                    value: "",
+                                },
+                                {
+                                    name: "string",
+                                    value: "",
+                                },
+                                {
+                                    name: "category",
+                                    value: "",
+                                },
+                            ]
+                        });
+                    }
+                    if (token.text === "Lookup") {
+                        annotations.push({
+                            name: "base",
+                            features: [
+                                {
+                                    name: "majorType",
+                                    value: "",
+                                },
+                                {
+                                    name: "minorType",
+                                    value: "",
+                                },
+                                {
+                                    name: "class",
+                                    value: "",
+                                },
+                            ]
+                        });
+                    }
                     
                     return Promise.resolve(
                         _.flatten(annotations.map(
